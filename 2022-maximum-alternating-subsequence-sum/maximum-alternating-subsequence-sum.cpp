7c0b06ab61a54bef3293c006eb8e6e2a58c2cfba
Harshit Gupta <139143163+harshitgupta2610@users.noreply.github.com>
2025-02-25 17:53:06 +0530

    Time: 301 ms (11.78%) | Memory: 168.2 MB (5.49%) - LeetSync

---
+++ 2022-maximum-alternating-subsequence-sum/maximum-alternating-subsequence-sum.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    long long helper(vector<int> &nums, int n , int idx, bool flag,vector<vector<long long>> &dp){
+        if(idx >= n) {
+            return 0;
+        }
+        if(dp[idx][flag]!=-1){
+            return dp[idx][flag];
+        }
+        long long skip= helper(nums, n, idx + 1 ,flag,dp);
+        long long val = nums[idx];
+        if(flag == false){
+            val = -val;
+        }
+        long long take = helper(nums, n, idx+1,!flag,dp)+val;
+
+        return dp[idx][flag]= max(take, skip);
+    }
+    long long maxAlternatingSum(vector<int>& nums) {
+        int n = nums.size();
+        int m = 2;
+        vector<vector<long long>> dp(n, vector<long long>(2, -1));
+        return helper(nums,n, 0, true,dp);
+    }
+};
