0dfb8b8c0a7d364943b567bc6e55c882573459a2
Harshit Gupta <139143163+harshitgupta2610@users.noreply.github.com>
2025-03-26 10:19:58 +0530

    Time: 16 ms (6.01%) | Memory: 19.3 MB (12.45%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.cpp
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -1,36 +1,35 @@
 class Solution {
 public:
-    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<int> indegree(numCourses,0);
-        queue<int> q;
-        vector<vector<int>> adj(numCourses);
-        vector<int> order;
-
-        for(auto i :prerequisites ){
+    vector<int> findOrder(int n, vector<vector<int>>& mp) {
+        unordered_map<int,vector<int>>adj;
+        vector<int> indegree(n);
+        vector<int> result;
+         for(auto i :mp ){
             int x= i[0];
             int y = i[1];
             adj[y].push_back(x);
             indegree[x]++;
         }
-        for(int  i=0;i<numCourses;i++){
+        queue<int> que;
+        for(int i =0 ;i <n;i++){
             if(indegree[i]==0){
-                q.push(i);
+                que.push(i);
             }
         }
-        while(!q.empty()){
-            int x=q.front();
-            order.push_back(x); 
-            q.pop();
-            for(int i =0;i<adj[x].size();i++){
-                indegree[adj[x][i]]--;
-                if(indegree[adj[x][i]]==0){
-                    q.push(adj[x][i]);
+        while(!que.empty()){
+            int curr = que.front();
+            result.push_back(curr);
+            que.pop();
+            for(int &v : adj[curr]){
+                indegree[v]--;
+                if(indegree[v]==0){
+                    que.push(v);
                 }
             }
         }
-        if (order.size() != numCourses) {
+        if (result.size()!=n) {
             return {};
         }
-        return order;
+        return result;
     }
 };
