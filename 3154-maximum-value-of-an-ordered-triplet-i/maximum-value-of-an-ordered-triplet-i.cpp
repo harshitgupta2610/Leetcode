baaad1d98a0d7e1bcab9d15088c5448b814dd4a2
Harshit Gupta <139143163+harshitgupta2610@users.noreply.github.com>
2025-04-02 16:44:38 +0530

    Time: 0 ms (100.00%) | Memory: 21.6 MB (88.55%) - LeetSync

--- 3154-maximum-value-of-an-ordered-triplet-i/maximum-value-of-an-ordered-triplet-i.cpp
+++ 3154-maximum-value-of-an-ordered-triplet-i/maximum-value-of-an-ordered-triplet-i.cpp
@@ -6,7 +6,7 @@ public:
         for(int i =0 ;i<n-2;i++){
             for(int j =i+1 ;j<n-1;j++){
                 for(int k =j+1 ;k<n;k++){
-                    long long x = (1LL * nums[i] - nums[j]) * nums[k];
+                    long long x = (1LL* nums[i] - nums[j]) * nums[k];
                     ans= max(ans,x);
                 }
             }
