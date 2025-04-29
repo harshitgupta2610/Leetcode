8fa4cf6ffa36d225ceec9575c3005fced4e903a5
Harshit Gupta <139143163+harshitgupta2610@users.noreply.github.com>
2025-04-29 07:12:57 +0530

    Time: 0 ms (100.00%) | Memory: 48.4 MB (45.25%) - LeetSync

--- 3685-count-subarrays-of-length-three-with-a-condition/count-subarrays-of-length-three-with-a-condition.cpp
+++ 3685-count-subarrays-of-length-three-with-a-condition/count-subarrays-of-length-three-with-a-condition.cpp
@@ -5,15 +5,20 @@ public:
         int i=0;
         int j=0;
         int count=0;
-        while(j<n){
-            if(j-i+1==3){
-                int mid = (j+i)/2;
-                if(2*(nums[i]+nums[j])==(nums[i+1])){
-                    count++;
-                }
-                i++;
+        // while(j<n){
+        //     if(j-i+1==3){
+        //         int mid = (j+i)/2;
+        //         if(2*(nums[i]+nums[j])==(nums[i+1])){
+        //             count++;
+        //         }
+        //         i++;
+        //     }
+        //     j++;
+        // }
+        for(int i =1;i<n-1;i++){
+            if(2*(nums[i-1]+nums[i+1])==(nums[i])){
+                count++;
             }
-            j++;
         }
         return count;
     }
