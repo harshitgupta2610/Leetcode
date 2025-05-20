aa2672d7f6fc5b96d98156a2a4584896888cca36
Harshit Gupta <139143163+harshitgupta2610@users.noreply.github.com>
2025-05-20 10:57:20 +0530

    Time: 10 ms (18.61%) | Memory: 15.5 MB (73.81%) - LeetSync

--- 2149-remove-colored-pieces-if-both-neighbors-are-the-same-color/remove-colored-pieces-if-both-neighbors-are-the-same-color.cpp
+++ 2149-remove-colored-pieces-if-both-neighbors-are-the-same-color/remove-colored-pieces-if-both-neighbors-are-the-same-color.cpp
@@ -6,15 +6,14 @@ public:
         int b=0;
 
         for(int i =0;i<n-2;i++){
-            
+            if(s[i+1]=='A'){
             if(s[i]==s[i+1]&& s[i+1]==s[i+2]){
-                if(s[i+1]=='A'){
-                    a++;
-                }
-                else if(s[i+1]=='B'){
-                    b++;
-                }
-            }
+                a++;
+            }}
+            else if(s[i+1]=='B'){
+            if(s[i]==s[i+1]&& s[i+1]==s[i+2]){
+                b++;
+            }}
         }
         return a>b;
     }
