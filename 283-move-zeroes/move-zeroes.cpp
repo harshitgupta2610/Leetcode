674ec2bdbb5fb8f032d36434b3dd04d90250cd3b
Harshit Gupta <139143163+harshitgupta2610@users.noreply.github.com>
2025-06-12 07:38:49 +0530

    Time: 0 ms (100.00%) | Memory: 23.9 MB (17.38%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -5,7 +5,7 @@ public:
         int pos =0;
         for(int i =0;i<n;i++){
             if(nums[i]!=0){
-               swap(nums[i], nums[pos]);
+                swap(nums[i], nums[pos]);
                 pos++;
             }
             
