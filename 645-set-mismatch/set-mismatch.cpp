e6a941d2d413de6eda7c412640f78972292cf25e
Harshit Gupta <139143163+harshitgupta2610@users.noreply.github.com>
2025-06-25 09:57:17 +0530

    Time: 73 ms (5.44%) | Memory: 36.8 MB (5.25%) - LeetSync

--- 645-set-mismatch/set-mismatch.cpp
+++ 645-set-mismatch/set-mismatch.cpp
@@ -1,22 +1,23 @@
-
 class Solution {
 public:
     vector<int> findErrorNums(vector<int>& nums) {
         int n =nums.size();
-        vector<int> v(n+1,0);
-        for(int i=0;i<n;i++){
-            v[nums[i]]++;
+        map<int,int> mp;
+        for(int i =0;i<n;i++){
+            mp[nums[i]]++;
         }
-        int mis =0;int dup =0;
-        for(int i=1;i<v.size();i++){
-            if(v[i]==2){
-                dup = i;
+        int dup =0;
+        int mis =0;
+        for(int i =1;i<=n;i++){
+            if(mp.count(i)){
+                if(mp[i]==2){
+                    dup = i;
+                }
             }
-            if(v[i]==0){
-                mis = i;
+            else{
+                mis=i;
             }
         }
         return {dup,mis};
     }
-}; 
-
+};
