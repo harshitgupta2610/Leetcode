4e82590c28777ae16ce6f02676e438a1c5d0cfa4
Harshit Gupta <139143163+harshitgupta2610@users.noreply.github.com>
2025-07-04 14:47:25 +0530

    Time: 306 ms (9.38%) | Memory: 163.9 MB (6.61%) - LeetSync

--- 646-maximum-length-of-pair-chain/maximum-length-of-pair-chain.cpp
+++ 646-maximum-length-of-pair-chain/maximum-length-of-pair-chain.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
-    int n ;
-    int helper(vector<vector<int>>& pairs,int idx, int prev, vector<vector<int>> &dp){
+    int n;
+    int helper(vector<vector<int>>& pairs,int idx ,int prev, vector<vector<int>> &dp ){
         if(idx>=n){
             return 0;
         }
         if(dp[idx][prev+1]!=-1){
             return dp[idx][prev+1];
         }
-        int take=0;
-        if(prev==-1 || pairs[idx][0]>pairs[prev][1]){
-            take = 1+ helper(pairs,idx+1,idx,dp);
+        int take= 0;
+        if(prev == -1 || pairs[idx][0]>pairs[prev][1]){
+            take= 1+ helper(pairs,idx+1,idx,dp);
         }
         int skip = helper(pairs,idx+1,prev,dp);
-        return dp[idx][prev+1]=max(take,skip);
+        return dp[idx][prev+1] = max(skip,take);
     }
     int findLongestChain(vector<vector<int>>& pairs) {
-        n=pairs.size();
+        n = pairs.size();
         vector<vector<int>> dp(n+2,vector<int>(n+2,-1));
         sort(pairs.begin(),pairs.end());
         return helper(pairs,0,-1,dp);
