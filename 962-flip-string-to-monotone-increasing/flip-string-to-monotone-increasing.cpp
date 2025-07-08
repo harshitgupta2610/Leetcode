5bb41c75bb093d5e9f69b9055dd1b30bb1096e3a
Harshit Gupta <139143163+harshitgupta2610@users.noreply.github.com>
2025-07-08 17:57:00 +0530

    Time: 4 ms (79.11%) | Memory: 13.5 MB (84.80%) - LeetSync

--- 962-flip-string-to-monotone-increasing/flip-string-to-monotone-increasing.cpp
+++ 962-flip-string-to-monotone-increasing/flip-string-to-monotone-increasing.cpp
@@ -1,36 +1,16 @@
 class Solution {
 public:
-    int n;
-    int solve(string &s,int idx, int prev,vector<vector<int>> &dp){
-        if(idx>=n){
-            return 0;
-        }
-        int flip = INT_MAX;
-        int noflip = INT_MAX;
-        if(dp[idx][prev]!=-1){
-            return dp[idx][prev];
-        }
-        if(s[idx]=='0'){
-            if(prev==1){
-                flip = 1 + solve(s,idx+1,1,dp);
-            }else{
-                flip = 1 + solve(s,idx+1,1,dp);
-                noflip = solve(s,idx+1,0,dp);
-            }
-        }
-        else if(s[idx]=='1'){
-            if(prev== 1 ){
-                noflip =solve(s,idx+1,1,dp);
-            }else{
-                flip = 1 + solve(s,idx+1,0,dp);
-                noflip = solve(s,idx+1,1,dp);
-            }
-        }
-        return dp[idx][prev]=min(flip,noflip);
-    }
     int minFlipsMonoIncr(string s) {
-        n =s.length();
-        vector<vector<int>> dp(n+1,vector<int> (2,-1));
-        return solve(s,0,0,dp);
+        int count1 =0;
+        int flip =0;
+        for(char ch : s){
+            if(ch=='1'){
+                count1++;
+            }
+            else{
+                flip = min(flip+1,count1);
+            }
+        }
+        return flip;
     }
 };
