2245b7eec7dbda411f314d388fa1051b38228d2e
Harshit Gupta <139143163+harshitgupta2610@users.noreply.github.com>
2025-07-12 11:08:25 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (33.71%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,45 +1,22 @@
 class Solution {
 public:
-
-
-    //recursion+memo
-
-
-    // int helper(int i , int j,int m, int n,vector<vector<int>> &dp){
-    //     if(i==m-1 && j==n-1){
-    //         return 1;
-    //     }
-    //     if(i<0 || i>=m || j<0 || j>=n){
-    //         return 0;
-    //     }
-    //     if(dp[i][j]!=0){
-    //         return dp[i][j];
-    //     }
-    //     int l = helper(i+1,j,m,n,dp);
-    //     int r = helper(i,j+1,m,n,dp);
-    //     return dp[i][j]= l+r;
-    // }
-    // int uniquePaths(int m, int n) {
-    //     vector<vector<int>> dp (m+1,vector<int> (n+1,0));
-    //     return helper(0,0,m,n,dp);
-    // }
-
-
-    //Bottomup
+    int solve(int i ,int j ,int  m,int n,vector<vector<int>> &dp){
+        if(i==m-1 && j == n-1){
+            return 1;
+        }
+        if(i < 0 || i >= m || j < 0 || j >= n) {
+            return 0;
+        }
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+        
+        int right = solve(i,j+1,m,n,dp);
+        int down  = solve(i+1,j,m,n,dp);
+        return dp[i][j] = right+down;
+    }
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp (m+1,vector<int> (n+1));
-        dp[0][0]=1;
-        for(int i =1;i<n;i++){
-            dp[0][i]=1;
-        }
-        for(int i =1;i<m;i++){
-            dp[i][0]=1;
-        }
-        for(int i =1;i<m;i++){
-            for(int j =1;j<n;j++){
-                dp[i][j]=dp[i][j-1]+dp[i-1][j];
-            }
-        }
-        return dp[m-1][n-1];
+        vector<vector<int>> dp(m+1,vector<int>(n+1,-1));
+        return solve(0,0,m,n, dp);
     }
 };
