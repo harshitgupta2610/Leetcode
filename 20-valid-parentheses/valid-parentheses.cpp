fc6d4537f22fd115e379f929141a25c5cbc8bb53
Harshit Gupta <139143163+harshitgupta2610@users.noreply.github.com>
2025-07-29 19:26:50 +0530

    Time: 2 ms (12.62%) | Memory: 8.7 MB (60.77%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     bool isValid(string s) {
         stack<char> st;
-        for(char ch:s) {
+        for(auto &ch : s){
             if(st.empty() || ch == '(' || ch == '{' || ch == '[') {
                 st.push(ch);
                 continue;
@@ -12,19 +12,20 @@ public:
                     st.pop();
                 else
                     return false;
-            } else if(ch == '}') {
+            }
+            else if(ch == '}') {
                 if(st.top() == '{')
                     st.pop();
                 else
                     return false;
-            } else if(ch == ']') {
+            }
+            else if(ch == ']') {
                 if(st.top() == '[')
                     st.pop();
                 else
                     return false;
             }
         }
-        
         return st.empty();
     }
 };
