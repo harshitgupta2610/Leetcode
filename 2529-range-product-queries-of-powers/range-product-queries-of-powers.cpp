cfb5cef1dd351dbe6f72c9da4093b9f30c2372da
Harshit Gupta <139143163+harshitgupta2610@users.noreply.github.com>
2025-08-11 18:17:26 +0530

    Time: 20 ms (61.70%) | Memory: 147.8 MB (50.53%) - LeetSync

---
+++ 2529-range-product-queries-of-powers/range-product-queries-of-powers.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int m = 1e9+7;
+    vector<int> productQueries(int n, vector<vector<int>>& queries) {
+        vector<int>powers;
+        vector<int> result;
+        for(int i=0;i<32;i++){
+            if((n&(1<<i))!=0){
+                powers.push_back(pow(2,i));//pow(2,i);
+            }
+        }
+        for(auto &query : queries){
+            int start = query[0];
+            int end = query[1];
+            long product =1;
+            for(int i = start;i<=end;i++){
+                product = (product*powers[i])%m;
+            }
+            result.push_back(product);
+        }
+        return result;
+    }
+};
