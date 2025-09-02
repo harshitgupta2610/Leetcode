ba7aa5f6ed20c781f83da3a7fe39f58f7145b3f3
Harshit Gupta <139143163+harshitgupta2610@users.noreply.github.com>
2025-09-02 09:20:29 +0530

    Time: 4 ms (5.75%) | Memory: 18.3 MB (47.37%) - LeetSync

--- 733-flood-fill/flood-fill.cpp
+++ 733-flood-fill/flood-fill.cpp
@@ -1,21 +1,38 @@
 class Solution {
 public:
-     void dfs(vector<vector<int>>& image, int i, int j,int iniColor, int newColor){
-        int m = image.size();
-        int n = image[0].size();
-        if(i<0 || i>=m || j<0 || j>=n || image[i][j] == newColor || image[i][j] != iniColor){
-            return;
-        }
-        image[i][j] = newColor;
-        dfs(image,i-1,j,iniColor,newColor);
-        dfs(image,i+1,j,iniColor,newColor);
-        dfs(image,i,j-1,iniColor,newColor);
-        dfs(image,i,j+1,iniColor,newColor);
-    }
-    
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor){
-        int iniColor = image[sr][sc];
-        dfs(image,sr,sc,iniColor,newColor);
+        int m = image.size(), n = image[0].size();
+
+        int ini = image[sr][sc];
+
+        if (ini == newColor) return image;          
+
+        queue<pair<int,int>> q;
+
+        q.push({sr, sc});
+
+        image[sr][sc] = newColor;                             
+        int dr[8] = {-1, 0, 1, 0};
+        int dc[8] = {0, 1, 0, -1};
+
+        while (!q.empty()) {
+
+            auto [r, c] = q.front(); 
+            
+            q.pop();
+            for (int k = 0; k < 4; k++) {                     
+
+                int nr = r + dr[k], nc = c + dc[k];
+
+                if (nr >= 0 && nr < m && nc >= 0 && nc < n   
+                    && image[nr][nc] == ini) {                
+
+                    image[nr][nc] = newColor;  
+
+                    q.push({nr, nc});                        
+                }
+            }
+        }
         return image;
     }
 };
