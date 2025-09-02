57c3b216e58e6ff9171208d049ca00baa2de6f92
Harshit Gupta <139143163+harshitgupta2610@users.noreply.github.com>
2025-09-02 14:57:34 +0530

    Time: 231 ms (70.07%) | Memory: 10.3 MB (94.31%) - LeetSync

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -2,31 +2,35 @@ class Solution {
 public:
     int n, m;
     
-    bool helper(int i, int j, vector<vector<char>>& board, string& word, int idx, vector<vector<int>>& visited) {
-        if (idx == word.size()) return true; 
-        
-        if (i < 0 || i >= n || j < 0 || j >= m || board[i][j] != word[idx] || visited[i][j])
+    bool helper(int i, int j, vector<vector<char>>& board, string& word, int idx) {
+        if(idx == word.length()){
+            return true;
+        }
+        if(i<0 || i>=m || j<0 || j>=n || board[i][j] == '$'){
             return false;
-        
-        visited[i][j] = 1; 
-        bool found = helper(i + 1, j, board, word, idx + 1, visited) ||
-                     helper(i - 1, j, board, word, idx + 1, visited) ||
-                     helper(i, j + 1, board, word, idx + 1, visited) ||
-                     helper(i, j - 1, board, word, idx + 1, visited);
-        
-        visited[i][j] = 0;
+        }
+        if(board[i][j]!=word[idx]){
+            return false;
+        }
+        char temp = board[i][j];
+        board[i][j] = '$';
+        bool found = helper(i + 1, j, board, word, idx + 1) ||
+                     helper(i - 1, j, board, word, idx + 1) ||
+                     helper(i, j + 1, board, word, idx + 1) ||
+                     helper(i, j - 1, board, word, idx + 1);
+
+        board[i][j] = temp;
         return found;
     }
 
     bool exist(vector<vector<char>>& board, string word) {
-        n = board.size();
-        m = board[0].size();
-        vector<vector<int>> visited(n, vector<int>(m, 0));
+        m = board.size();
+        n = board[0].size();
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (board[i][j] == word[0]) { 
-                    if (helper(i, j, board, word, 0, visited)) return true;
+        for(int i =0;i<m;i++){
+            for(int j =0;j<n;j++){
+                if(board[i][j]== word[0] && helper(i,j,board,word,0)){
+                    return true;
                 }
             }
         }
