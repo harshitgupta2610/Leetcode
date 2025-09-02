a2c7436576325282e4f3be253020d9495a7193e1
Harshit Gupta <139143163+harshitgupta2610@users.noreply.github.com>
2025-09-02 14:23:18 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (79.24%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.cpp
+++ 54-spiral-matrix/spiral-matrix.cpp
@@ -3,46 +3,29 @@ public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
         int m = matrix.size();
         int n = matrix[0].size();
-
-        /*
-        dir -0 left -right
-        dir -1 top to bottom
-        dir -2 right to left
-        dir -3 bottom to top
-        */
-
-        vector<int> result ;
-        int dir =0;
-        int top =0;
-        int bottom = m-1;
-        int left =0;
-        int right =n-1;
-
-        while(top <= bottom && left <= right){
+        vector<int> result;
+        int dir =0 , top= 0 ,bottom = m-1, left =0 , right = n-1;
+        while(top<=bottom && left<=right){
             if(dir == 0 ){
-                //left  to right
-                for(int i=left;i<=right;i++){
+                for(int i = left ;i<=right ;i++){
                     result.push_back(matrix[top][i]);
                 }
                 top++;
             }
-            else if(dir == 1 ){
-                //top to bottom
-                for(int i=top;i<=bottom;i++){
+            else if(dir == 1){
+                for(int i = top ;i<=bottom ;i++){
                     result.push_back(matrix[i][right]);
                 }
                 right--;
             }
-            else if(dir == 2 ){
-                //right to left
-                for(int i=right;i>=left;i--){
+            else if(dir == 2){
+                for(int i = right ;i>=left ;i--){
                     result.push_back(matrix[bottom][i]);
                 }
                 bottom--;
             }
             else if(dir == 3){
-                //bottom to top
-                for(int i=bottom;i>=top;i--){
+                for(int i = bottom ;i>=top ;i--){
                     result.push_back(matrix[i][left]);
                 }
                 left++;
