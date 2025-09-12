e1ae7bb80ea89a193c202f5cefc4802cf4993b39
Harshit Gupta <139143163+harshitgupta2610@users.noreply.github.com>
2025-09-12 20:08:47 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (73.41%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,17 +1,22 @@
 class Solution {
 public:
-    int t [50];
-    int helper(int n){
-       if(n==0 || n==1 || n==2){
-        return n;
-       }
-       if(t[n]!=-1){
-        return t[n];
-       }
-        return  t[n]=  helper(n-1) +helper(n-2);
+    int t[50];
+    int solve(int n){
+        if(n<0){
+            return 0;
+        }
+        if(n==0||n==1||n==2){
+            return n;
+        }
+        if(t[n]!=-1){
+            return t[n];
+        }
+        int one_step = solve(n-1);
+        int two_step = solve(n-2);
+        return t[n] =  one_step + two_step ;
     }
     int climbStairs(int n) {
         memset(t,-1,sizeof(t));
-        return helper(n);
+        return solve(n);
     }
 };
